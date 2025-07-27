11f7ef894976a14f62404af653cd898ca433e78f
hanifubaaba <baabahanif88@gmail.com>
2025-07-27 01:24:01 +0100

    modified views register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('library/int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login', UserLoginView.as_view(), name = 'login'),
     path('logout', UserLogoutView.as_view(), name = 'logout'),
-    path('register', UserRegistrationView.as_view(), name = 'register')
+    path('views.register', UserRegistrationView.as_view(), name = 'register')
 ]
