8559aba4d21c860a55083320b350319c3c1fc750
hanifubaaba <baabahanif88@gmail.com>
2025-07-27 14:31:19 +0100

    added / for proper routing

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, UserRegistrationView
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', views.books_view, name='books'),
-    path('library/int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login', LoginView.as_view(template_name = 'relationship_app/login.html'), name = 'login'),
-    path('logout', LogoutView.as_view(template_name = 'relationship_app/logout.html'), name = 'logout'),
-    path('views.register', UserRegistrationView.as_view(template_name = 'register.html'), name = 'register')
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name ='relationship_app/login.html'), name = 'login'),
+    path('logout/', LogoutView.as_view(template_name = 'relationship_app/logout.html'), name = 'logout'),
+    path('register/', UserRegistrationView.as_view(template_name = 'relationship_app/register.html'), name = 'register')
 ]
