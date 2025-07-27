4c243c02cf025c8484312bf653540f06aa14c51b
hanifubaaba <baabahanif88@gmail.com>
2025-07-27 14:22:54 +0100

    added template_name in url patterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import list_books
+from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', views.books_view, name='books'),
     path('library/int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login', LoginView.as_view('login.html'), name = 'login'),
-    path('logout', LogoutView.as_view('logout.html'), name = 'logout'),
-    path('views.register', UserRegistrationView.as_view('register.html'), name = 'register')
+    path('login', LoginView.as_view(template_name = 'login.html'), name = 'login'),
+    path('logout', LogoutView.as_view(template_name = 'logout.html'), name = 'logout'),
+    path('views.register', UserRegistrationView.as_view(template_name = 'register.html'), name = 'register')
 ]
