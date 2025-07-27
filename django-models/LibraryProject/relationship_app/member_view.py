f609aa14716d8e9c5df778b4ef735e0ca5d010b2
hanifubaaba <baabahanif88@gmail.com>
2025-07-27 15:28:51 +0100

    >

--- django-models/LibraryProject/relationship_app/member_view.py
+++ django-models/LibraryProject/relationship_app/member_view.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
 def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    return hasattr(user, 'UserProfile') and user.userprofile.role == 'Member'
 
 @user_passes_test(is_member)
 def member_view(request):
