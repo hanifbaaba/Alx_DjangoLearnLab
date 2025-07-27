0ec98e9ac1affb9b4b5b8bb4ba04df52bf866773
hanifubaaba <baabahanif88@gmail.com>
2025-07-27 01:21:16 +0100

    imported auth config for user login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.contrib.auth import login
 
 
 def books_view(request):
