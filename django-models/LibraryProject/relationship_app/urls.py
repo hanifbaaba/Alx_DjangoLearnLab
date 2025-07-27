298cc8e221e55e0e87e5b38d3cb621de3f8efbf6
hanifubaaba <baabahanif88@gmail.com>
2025-07-27 14:25:14 +0100

    modified url patterns to the correct template name

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('books/', views.books_view, name='books'),
     path('library/int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login', LoginView.as_view(template_name = 'login.html'), name = 'login'),
-    path('logout', LogoutView.as_view(template_name = 'logout.html'), name = 'logout'),
+    path('login', LoginView.as_view(template_name = 'relationship_app/login.html'), name = 'login'),
+    path('logout', LogoutView.as_view(template_name = 'relationship_app/logout.html'), name = 'logout'),
     path('views.register', UserRegistrationView.as_view(template_name = 'register.html'), name = 'register')
 ]
