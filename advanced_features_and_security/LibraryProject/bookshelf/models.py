e293ce2d89dc6f200f49b5e8951318772f291ec1
hanifubaaba <baabahanif88@gmail.com>
2025-07-30 03:29:45 +0100

    .

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,7 +3,26 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+class User(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile/")
+   
 
+  
+class UserManager(BaseUserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("User must have email address")
+        user = self.model(self.normalize_email(email))
+        user.set_password(password)
+        user.save(using = user._db)
+        return user
+    
+    def create_superuser(self, email, password):
+        user =self.crete_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 class Book(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length=100)
@@ -57,23 +76,3 @@ class UserProfile(models.Model):
             ("can_delete_book", "Can delete a book"),
         ]
 
-class User(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to="profile/")
-   
-
-  
-class UserManager(BaseUserManager):
-    def create_user(self, email, password):
-        if not email:
-            raise ValueError("User must have email address")
-        user = self.model(self.normalize_email(email))
-        user.set_password(password)
-        user.save(using = user._db)
-        return user
-    
-    def create_superuser(self, email, password):
-        user =self.crete_user(email, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
