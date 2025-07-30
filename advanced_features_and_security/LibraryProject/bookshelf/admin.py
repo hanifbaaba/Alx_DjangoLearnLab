c4bd9eb4dbf23451d0342043bf831c344f5c168b
hanifubaaba <baabahanif88@gmail.com>
2025-07-30 03:28:00 +0100

    modified advanced features app

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import User
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title','author', 'publication_year')
@@ -7,3 +9,21 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ("author",)
 
 admin.site.register(Book, BookAdmin)
+
+
+
+
+class UserAdmin(DefaultUserAdmin):
+    model = User
+    fieldsets = (
+        DefaultUserAdmin.fieldsets + (
+            (None, {'fields': ('date_of_birth', 'profile_photo')})
+        )
+
+    )
+    add_fieldsets = (
+        DefaultUserAdmin.add_fieldsets + (None, {'fields':('date_of_birth')})
+    )
+
+
+admin.site.register(User, UserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,79 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 
 class Book(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+
+class Author(models.Model):
+  name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+class Book(models.Model):
+  title = models.CharField(max_length=100)
+  author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+  def __str__(self):
+    return self.title
+
+class Library(models.Model):
+  name = models.CharField(max_length=100)
+  books = models.ManyToManyField(Book, related_name='books')
+
+  def __str__(self):
+    return self.name
+
+class Librarian(models.Model):
+  name = models.CharField(max_length=100)
+  library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
+  
+ROLE_CHOICES = (
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username} ({self.role})'
+    
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
+
+class User(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile/")
+   
+
+  
+class UserManager(BaseUserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("User must have email address")
+        user = self.model(self.normalize_email(email))
+        user.set_password(password)
+        user.save(using = user._db)
+        return user
+    
+    def create_superuser(self, email, password):
+        user =self.crete_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
