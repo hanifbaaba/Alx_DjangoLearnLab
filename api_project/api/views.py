4f8cd561765567d2b7ccef67d1f057659d48e158
hanifubaaba <baabahanif88@gmail.com>
2025-08-03 01:53:01 +0100

    configured urls in api_project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
-# from django.shortcuts import render
-# from .serializers import BookSerializer
-# # from django.views.generic.detail import 
-# # Create your views here.
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
 
-# class BookList(generics.ListAPIView):
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
