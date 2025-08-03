fcf9fb60cdd5f7c00e55017233ba413fea9132a8
hanifubaaba <baabahanif88@gmail.com>
2025-08-03 01:49:00 +0100

    created serializers component

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from .serializers import BookSerializer
+# # from django.views.generic.detail import 
+# # Create your views here.
 
-# Create your views here.
+# class BookList(generics.ListAPIView):
