f8e1fd581d2dcce5cfc157cac93fdbb2458df0e2
hanifubaaba <baabahanif88@gmail.com>
2025-08-04 20:39:00 +0100

    .

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics, mixins, permissions
 from .models import Book
 from .serializers import BookSerializer
