8d537081fe29d28cabe466afab34370455402fd1
hanifubaaba <baabahanif88@gmail.com>
2025-08-14 00:22:47 +0100

    .

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Post
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -15,3 +16,13 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class PostForm(forms.ModelForm):
+  class Meta:
+    model = Post
+    fields = ["title", "content"]
+    widgets = {
+      "title": forms.TextInput(attrs={"placeholder": "Post title"}),
+      "content": forms.Textarea(attrs={"rows": 8, "placeholder": "Write your post..."}),
+    }
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length = 200)
@@ -17,4 +17,8 @@ class Profile(models.Model):
     profile_picture = models.ImageField(upload_to="profile_pics", blank=True, null=True )
 
 def __str__(self):
-    return self.title
+    return f"{self.user.name} Profile"
+
+
+def get_absolute_url(self):
+    return reverse('post-detail', kwargs={'pk': self.pk}) 
