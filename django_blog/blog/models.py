e787f6bbe6cfb7b49b0afea731a748be60fef375
hanifubaaba <baabahanif88@gmail.com>
2025-08-17 19:35:50 +0100

    added Tag class in db models

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length = 200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    tags = TaggableManager()
 
 def __str__(self):
     return self.title
@@ -35,3 +37,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
+    
+
+class Tag(models.Model):
+     name = models.ManyToManyField(Post, max_length = 100)
