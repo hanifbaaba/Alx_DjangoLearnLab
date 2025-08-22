9016e7935461bf67f7ecfc353148bd1834b4fdd1
hanifubaaba <baabahanif88@gmail.com>
2025-08-22 18:21:25 +0100

    Configured UserFeedView in urls

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostCreateView, CommentCreateView
+from .views import PostCreateView, CommentCreateView, UserFeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostCreateView, basename='post')
 router.register(r'comments', CommentCreateView, basename='comment')
 urlpatterns = router.urls
+
+
+urlpatterns = [
+  path('feed/', UserFeedView.as_view(), name='feed'),
+] + router.urls
