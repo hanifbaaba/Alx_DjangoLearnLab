a60f8efe0defb0a2126f9d41c5f0f9f6f5940545
hanifubaaba <baabahanif88@gmail.com>
2025-08-23 00:48:32 +0100

    created a model for Likes

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 
-# User = settings.AUTH_USER_MODEL
+User = settings.AUTH_USER_MODEL
 
 class Post(models.Model):
     author = models.ForeignKey( settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
@@ -27,3 +27,13 @@ class Comment(models.Model):
     def __str__(self):
         return f"Comment by {self.author} on {self.post.title}"
     
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("post", "user") 
+
+    def __str__(self):
+         return f"{self.user.username} liked {self.post.id}"
