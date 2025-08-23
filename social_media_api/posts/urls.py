eee2211cb2c6d2237514331205de07211dedfb8f
hanifubaaba <baabahanif88@gmail.com>
2025-08-23 22:56:34 +0100

    Implemented url endpoints for liking and unliking posts

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostCreateView, CommentCreateView, UserFeedView
+from .views import PostCreateView, CommentCreateView, UserFeedView, LikePostView, UnlikePostView
 
 router = DefaultRouter()
 router.register(r'posts', PostCreateView, basename='post')
@@ -10,4 +10,7 @@ urlpatterns = router.urls
 
 urlpatterns = [
   path('feed/', UserFeedView.as_view(), name='feed'),
+  path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+  path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ] + router.urls
+
