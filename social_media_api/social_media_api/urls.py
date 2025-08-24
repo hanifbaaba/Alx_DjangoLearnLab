ff2895ae2079f6c9cabfc6413618c9c015de5e91
hanifubaaba <baabahanif88@gmail.com>
2025-08-24 15:07:29 +0100

    Added a homepage

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Welcome to Social Media API 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home), 
     path('api/', include('posts.urls'))
 ]
+
